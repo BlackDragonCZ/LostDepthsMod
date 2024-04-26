6cf6687fd0e04e47017b77ae82a88f18d703bc0f
BlackDragonCZ <31632485+BlackDragonCZ@users.noreply.github.com>
2024-04-26 23:34:52 +0200

    Sign drop rate fix

--- src/main/java/cz/blackdragoncz/lostdepths/block/decor/InfusedSignBlock.java
+++ src/main/java/cz/blackdragoncz/lostdepths/block/decor/InfusedSignBlock.java
@@ -1,21 +1,37 @@
 package cz.blackdragoncz.lostdepths.block.decor;
 
 import cz.blackdragoncz.lostdepths.init.LostdepthsModBlockEntities;
+import cz.blackdragoncz.lostdepths.init.LostdepthsModBlocks;
 import cz.blackdragoncz.lostdepths.init.LostdepthsModWoodTypes;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.StandingSignBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.loot.LootParams;
+
+import java.util.Collections;
+import java.util.List;
 
 public class InfusedSignBlock extends StandingSignBlock {
 
     public InfusedSignBlock() {
-        super(Properties.copy(Blocks.OAK_SIGN), LostdepthsModWoodTypes.INFUSED_IRON);
+        super(BlockBehaviour.Properties.of().sound(SoundType.METAL).strength(2).explosionResistance(2), LostdepthsModWoodTypes.INFUSED_IRON);
     }
 
     @Override
     public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
       return LostdepthsModBlockEntities.INFUSED_SIGN.get().create(pos, state);
+    }
+
+    @Override
+    public List<ItemStack> getDrops(BlockState state, LootParams.Builder builder) {
+        List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+        if (!dropsOriginal.isEmpty())
+            return dropsOriginal;
+        return Collections.singletonList(new ItemStack(this, 1));
     }
 }
--- src/main/java/cz/blackdragoncz/lostdepths/block/decor/InfusedStandingSignBlock.java
+++ src/main/java/cz/blackdragoncz/lostdepths/block/decor/InfusedStandingSignBlock.java
@@ -3,19 +3,32 @@ package cz.blackdragoncz.lostdepths.block.decor;
 import cz.blackdragoncz.lostdepths.block.entity.InfusedSignBlockEntity;
 import cz.blackdragoncz.lostdepths.init.LostdepthsModWoodTypes;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.StandingSignBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.loot.LootParams;
+
+import java.util.Collections;
+import java.util.List;
 
 public class InfusedStandingSignBlock extends StandingSignBlock {
     public InfusedStandingSignBlock() {
-        super(BlockBehaviour.Properties.of().sound(SoundType.METAL), LostdepthsModWoodTypes.INFUSED_IRON);
+        super(BlockBehaviour.Properties.of().sound(SoundType.METAL).strength(2).explosionResistance(2), LostdepthsModWoodTypes.INFUSED_IRON);
     }
 
     @Override
     public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
         return new InfusedSignBlockEntity(pos, state);
+    }
+
+    @Override
+    public List<ItemStack> getDrops(BlockState state, LootParams.Builder builder) {
+        List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+        if (!dropsOriginal.isEmpty())
+            return dropsOriginal;
+        return Collections.singletonList(new ItemStack(this, 1));
     }
 }
