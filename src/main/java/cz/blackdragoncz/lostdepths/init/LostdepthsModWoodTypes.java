58cbad5c5f90dd7f41e8ec3d9fd9c25b7ea54292
Sam <michalconstantine@gmail.com>
2024-05-02 21:57:45 +0200

    Removed sheet initialization

--- src/main/java/cz/blackdragoncz/lostdepths/init/LostdepthsModWoodTypes.java
+++ src/main/java/cz/blackdragoncz/lostdepths/init/LostdepthsModWoodTypes.java
@@ -1,13 +1,9 @@
 package cz.blackdragoncz.lostdepths.init;
 
-import net.minecraft.client.renderer.Sheets;
 import net.minecraft.world.level.block.state.properties.BlockSetType;
 import net.minecraft.world.level.block.state.properties.WoodType;
 
 public class LostdepthsModWoodTypes {
 
     public static WoodType INFUSED_IRON = WoodType.register(new WoodType("lostdepths:infused_iron", BlockSetType.IRON));
-    static {
-        Sheets.addWoodType(INFUSED_IRON);
-    }
 }
