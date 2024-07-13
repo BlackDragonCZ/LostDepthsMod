3dee68eb317a1ec1beafc3550fc406e31446571c
Sam <michalconstantine@gmail.com>
2024-07-13 23:26:15 +0200

    Removed print

--- src/main/java/cz/blackdragoncz/lostdepths/item/tool/AspectOfTheStar.java
+++ src/main/java/cz/blackdragoncz/lostdepths/item/tool/AspectOfTheStar.java
@@ -125,8 +125,6 @@ public class AspectOfTheStar extends SwordItem {
             for (LivingEntity entity : entities) {
                 entity.hurt(new DamageSource(level.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.INDIRECT_MAGIC)), entity.getMaxHealth() * 0.02f);
             }
-
-            System.out.println("Found " + entities.size() + " entities");
         }
 
         return ar;
