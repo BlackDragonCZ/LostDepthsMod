86489f7949410d8f4bb5d4d5f01f48e107e6d535
Sam <michalconstantine@gmail.com>
2024-07-18 23:20:32 +0200

    Implemented custom hold pose for flux lantern

--- src/main/java/cz/blackdragoncz/lostdepths/client/ClientSide.java
+++ src/main/java/cz/blackdragoncz/lostdepths/client/ClientSide.java
@@ -3,10 +3,15 @@ package cz.blackdragoncz.lostdepths.client;
 import cz.blackdragoncz.lostdepths.client.renderer.block.AlloyWorkstationRenderer;
 import cz.blackdragoncz.lostdepths.client.renderer.block.GalacticWorkstationRenderer;
 import cz.blackdragoncz.lostdepths.init.LostdepthsModBlockEntities;
+import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 import net.minecraft.client.renderer.blockentity.HangingSignRenderer;
 import net.minecraft.client.renderer.blockentity.SignRenderer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.client.event.RenderPlayerEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -18,6 +23,24 @@ public class ClientSide {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         modEventBus.addListener(this::registerEntityRenderers);
+        MinecraftForge.EVENT_BUS.addListener(this::renderPlayer);
+    }
+
+    public void renderPlayer(RenderPlayerEvent.Pre event)
+    {
+        if (isCustomHoldPose(event.getEntity(), InteractionHand.MAIN_HAND))
+        {
+            event.getRenderer().getModel().rightArmPose = HumanoidModel.ArmPose.BOW_AND_ARROW;
+        }
+
+        if (isCustomHoldPose(event.getEntity(), InteractionHand.OFF_HAND))
+        {
+            event.getRenderer().getModel().leftArmPose = HumanoidModel.ArmPose.BOW_AND_ARROW;
+        }
+    }
+
+    private boolean isCustomHoldPose(Player player, InteractionHand hand) {
+        return player.getItemInHand(hand).getItem() instanceof ICustomHoldPose;
     }
 
     public void registerEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
