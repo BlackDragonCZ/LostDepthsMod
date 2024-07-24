1f15708a8f15e25e1f55c30cb7d71a10da2719a2
Sam <michalconstantine@gmail.com>
2024-07-24 21:55:22 +0200

    Added a hack to play dead sound from BladeOfForgottenItem

--- src/main/java/cz/blackdragoncz/lostdepths/item/tool/BladeOfForgottenItem.java
+++ src/main/java/cz/blackdragoncz/lostdepths/item/tool/BladeOfForgottenItem.java
@@ -1,6 +1,8 @@
 
 package cz.blackdragoncz.lostdepths.item.tool;
 
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.TooltipFlag;
@@ -11,6 +13,8 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.network.chat.Component;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.List;
 
 import cz.blackdragoncz.lostdepths.procedures.BladeOfForgottenLivingEntityIsHitWithToolProcedure;
@@ -27,7 +31,7 @@ public class BladeOfForgottenItem extends SwordItem {
 			}
 
 			public float getAttackDamageBonus() {
-				return -0.5f;
+				return 0.0f;
 			}
 
 			public int getLevel() {
@@ -41,13 +45,26 @@ public class BladeOfForgottenItem extends SwordItem {
 			public Ingredient getRepairIngredient() {
 				return Ingredient.of();
 			}
-		}, 3, -2.4f, new Item.Properties().fireResistant());
+		}, 0, -2.4f, new Item.Properties().fireResistant());
 	}
 
 	@Override
 	public boolean hurtEnemy(ItemStack itemstack, LivingEntity entity, LivingEntity sourceentity) {
 		boolean retval = super.hurtEnemy(itemstack, entity, sourceentity);
 		BladeOfForgottenLivingEntityIsHitWithToolProcedure.execute(entity, sourceentity);
+
+		if (entity.isDeadOrDying() && entity instanceof Mob mob)
+		{
+            try {
+                Method method = LivingEntity.class.getDeclaredMethod("getDeathSound");
+				method.setAccessible(true);
+				entity.playSound((SoundEvent) method.invoke(mob), 1, 1);
+            } catch (NoSuchMethodException e) {
+            } catch (InvocationTargetException e) {
+            } catch (IllegalAccessException e) {
+            }
+		}
+
 		return retval;
 	}
 
