814b2acbfa92b017d2c80c1981db0119e3afd458
Sam <michalconstantine@gmail.com>
2024-08-11 10:43:18 +0200

    Finished item use recipe

--- src/main/java/cz/blackdragoncz/lostdepths/recipe/ItemUseRecipe.java
+++ src/main/java/cz/blackdragoncz/lostdepths/recipe/ItemUseRecipe.java
@@ -5,20 +5,29 @@ import cz.blackdragoncz.lostdepths.init.LostDepthsModRecipeType;
 import cz.blackdragoncz.lostdepths.init.LostdepthsModRecipeSerializers;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.GsonHelper;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.item.crafting.ShapedRecipe;
 import net.minecraftforge.items.wrapper.RecipeWrapper;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class ItemUseRecipe extends LDRecipe<RecipeWrapper> {
 
-    private ItemStack requiredItemInHand;
+    private ItemStack useItem;
     private ItemStack useOnItem;
+    private ItemStack result;
+    private String useDescription;
 
-    public ItemUseRecipe(ResourceLocation id) {
+    public ItemUseRecipe(ResourceLocation id, ItemStack useItem, ItemStack useOnItem, ItemStack result, String useDescription) {
         super(id);
+        this.useItem = useItem;
+        this.useOnItem = useOnItem;
+        this.result = result;
+        this.useDescription = useDescription;
     }
 
     @Override
@@ -36,22 +45,44 @@ public class ItemUseRecipe extends LDRecipe<RecipeWrapper> {
         return false;
     }
 
+    public ItemStack getUseItem() {
+        return useItem;
+    }
+
+    public ItemStack getUseOnItem() {
+        return useOnItem;
+    }
+
+    public ItemStack getResult() {
+        return result;
+    }
+
+    public String getUseDescription() {
+        return useDescription;
+    }
+
     public static class Serializer implements RecipeSerializer<ItemUseRecipe> {
 
         @Override
         public ItemUseRecipe fromJson(ResourceLocation id, JsonObject json) {
+            ItemStack useItem = ShapedRecipe.itemStackFromJson(GsonHelper.getAsJsonObject(json, "use_item"));
+            ItemStack useOnItem = ShapedRecipe.itemStackFromJson(GsonHelper.getAsJsonObject(json, "use_on"));
+            ItemStack result = ShapedRecipe.itemStackFromJson(GsonHelper.getAsJsonObject(json, "result"));
 
-            return new ItemUseRecipe(id);
+            return new ItemUseRecipe(id, useItem, useOnItem, result, GsonHelper.getAsString(json, "use_description"));
         }
 
         @Override
         public @Nullable ItemUseRecipe fromNetwork(ResourceLocation id, FriendlyByteBuf buf) {
-            return new ItemUseRecipe(id);
+            return new ItemUseRecipe(id, buf.readItem(), buf.readItem(), buf.readItem(), buf.readUtf());
         }
 
         @Override
         public void toNetwork(FriendlyByteBuf buf, ItemUseRecipe recipe) {
-
+            buf.writeItem(recipe.useItem);
+            buf.writeItem(recipe.useOnItem);
+            buf.writeItem(recipe.result);
+            buf.writeUtf(recipe.useDescription);
         }
     }
 }
